62bf365009436aaeb2de939328ccca5d71289c96
Tyler Fast <mrfast7@gmail.com>
2022-12-03 19:30:45 -0700

    Added md to html converter

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from re import search
 from . import util
+from markdown2 import Markdown
 
+def convert_to_html(title):
+    content = util.get_entry(title)
+    markdowner = Markdown()
+    if content == None:
+        return None
+    return markdowner.convert(content)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
 
 def search_view(request):
     query_dict = request.GET
@@ -14,15 +22,22 @@ def search_view(request):
     entries_list = util.list_entries()
     searched_entries = []
 
+    message = "No results found"
+
     for entry in entries_list:
         if query.lower() == entry.lower():
             return render(request, "encyclopedia/title.html", {
-            "entry": util.get_entry(query),
+            "entry": convert_to_html(query),
             "title": title         
             })
 
         if query.lower() in entry.lower():
             searched_entries.append(entry)
+        
+        else:
+            return render(request, "encyclopedia/search.html", {
+            "message": message
+            })
 
     return render(request, "encyclopedia/search.html", {
     "entries": searched_entries
@@ -34,6 +49,6 @@ def title(request, entry):
         return render(request, "encyclopedia/error.html")
 
     return render(request, "encyclopedia/title.html", {
-        "entry": util.get_entry(entry),
+        "entry": convert_to_html(entry),
         "title": title
     })
